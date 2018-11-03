9d4c75532f58c3cf3f7f17596a0ba99420f5bc88
wsdboss <395704469@qq.com>
2018-11-03 20:56:35 +0800

    no message

--- my-project/src/components/vue-ele/home/zzs.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <li>{{this.$route.params.id}}</li>
-</template>
-<script>
-    export default{
-     name:"zzs"
-    }
-</script>
--- my-project/src/store/actions.js
+++
@@ -1,8 +0,0 @@
-export default {
-    //context:上下文,类似store对象
-    asyReduce(context) {
-        //触发mutations函数
-       
-
-    }
-}
